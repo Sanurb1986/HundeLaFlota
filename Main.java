2d740b7918cf22af6f37ed3f0697a7fc73e40acf
Santiago Urbano Bedoya <santiagourbao@gmail.com>
2026-01-15 15:50:03 +0100

    hola

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ package HundeLaFlota;
 public class Main {
     public static void main(String[] args) {
         System.out.println("¡Bienvenido a Hunde La Flota!");
+        System.out.println("hola");
     }
 }
--- main.java
+++
@@ -1,7 +0,0 @@
-package HundeLaFlota;
-
-public class main {
-    public static void main(String[] args) {
-        System.out.println("¡Bienvenido a Hunde La Flota!");
-    }
-}
